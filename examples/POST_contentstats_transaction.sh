7c83435979ffc3c77031ac20e73ed03f0f01ccdc
patrickfrey <patrickpfrey@yahoo.com>
2018-05-30 11:50:11 +0200

    added some schemas

---
+++ examples/POST_contentstats_transaction.sh
@@ -0,0 +1,2 @@
+curl -i -H "Accept: text/plain" -X POST "http://127.0.0.1:8080/contentstats/reuters/transaction"
+
--- examples/buildAll.sh
+++ examples/buildAll.sh
@@ -9,6 +9,16 @@ curl -d "@examples/storage.json" -i -H "Accept: text/plain" -H "Content-Type: ap
 echo "--- Create inserter ---"
 curl -d "@examples/inserter.json" -i -H "Accept: text/plain" -H "Content-Type: application/json; charset=UTF-8" -X PUT "http://127.0.0.1:8080/inserter/reuters"
 
+echo "--- Create content statistics analyzer ---"
+curl -d "@examples/contentstats.json" -i -H "Accept: text/plain" -H "Content-Type: application/json; charset=UTF-8" -X PUT "http://127.0.0.1:8080/contentstats/reuters"
+
+echo "--- Analyze documents ---"
+TRANSACTION=`curl -H "Accept: text/plain" -X POST "http://127.0.0.1:8080/contentstats/reuters/transaction"`
+for dd in `find tests/data/reuters/doc/ -name "reut2-000.xml"`; do
+	echo "--- Collect statistics $dd ---"
+	curl -d "@$dd" -i -H "Accept: text/plain" -H "Content-Type: application/xml; charset=UTF-8" -X PUT "$TRANSACTION"
+done
+
 echo "--- Insert documents ---"
 TRANSACTION=`curl -H "Accept: text/plain" -X POST "http://127.0.0.1:8080/inserter/reuters/transaction"`
 for dd in `find tests/data/reuters/doc/ -name "*.xml"`; do
@@ -16,4 +26,5 @@ for dd in `find tests/data/reuters/doc/ -name "*.xml"`; do
 	curl -d "@$dd" -i -H "Accept: text/plain" -H "Content-Type: application/xml; charset=UTF-8" -X PUT "$TRANSACTION"
 done
 curl -i -H "Accept: text/plain" -H "Content-Type: application/xml; charset=UTF-8" -X PUT "$TRANSACTION"
+
 
