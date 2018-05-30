0fba6a9b0ff167f3d1e9d724715ae6965a3fe0fd
patrickfrey <patrickpfrey@yahoo.com>
2018-05-30 13:37:22 +0200

    demo script update

--- examples/buildAll_demo.sh
+++ examples/buildAll_demo.sh
@@ -1,17 +1,18 @@
 #!/bin/sh
 
 SERVER="http://demo.project-strus.net/reuters"
+EXAMPLES="https://raw.githubusercontent.com/patrickfrey/strusWebService/master/examples"
 
+mkdir -p examples
+cd examples
+wget $EXAMPLES/docanalyzer.json
+wget $EXAMPLES/storage.json
+wget $EXAMPLES/inserter.json
+wget $EXAMPLES/contentstats.json
+wget $EXAMPLES/query.orig.json
+cd ..
 wget "http://demo.project-strus.net/doc.tar.gz"
-
-echo "--- Create analyzer ---"
-curl -d "@examples/docanalyzer.json" -i -H "Accept: text/plain" -H "Content-Type: application/json; charset=UTF-8" -X PUT "$SERVER/docanalyzer/reuters"
-
-echo "--- Create storage ---"
-curl -d "@examples/storage.json" -i -H "Accept: text/plain" -H "Content-Type: application/json; charset=UTF-8" -X PUT "$SERVER/storage/reuters"
-
-echo "--- Create inserter ---"
-curl -d "@examples/inserter.json" -i -H "Accept: text/plain" -H "Content-Type: application/json; charset=UTF-8" -X PUT "$SERVER/inserter/reuters"
+tar -xvzf doc.tar.gz
 
 echo "--- Create content statistics analyzer ---"
 curl -d "@examples/contentstats.json" -i -H "Accept: text/plain" -H "Content-Type: application/json; charset=UTF-8" -X PUT "$SERVER/contentstats/reuters"
@@ -22,6 +23,16 @@ for dd in `find tests/data/reuters/doc/ -name "*.xml"`; do
 	echo "--- Collect statistics $dd ---"
 	curl -d "@$dd" -i -H "Accept: text/plain" -H "Content-Type: application/xml; charset=UTF-8" -X PUT "$TRANSACTION"
 done
+echo $TRANSACTION
+
+echo "--- Create analyzer ---"
+curl -d "@examples/docanalyzer.json" -i -H "Accept: text/plain" -H "Content-Type: application/json; charset=UTF-8" -X PUT "$SERVER/docanalyzer/reuters"
+
+echo "--- Create storage ---"
+curl -d "@examples/storage.json" -i -H "Accept: text/plain" -H "Content-Type: application/json; charset=UTF-8" -X PUT "$SERVER/storage/reuters"
+
+echo "--- Create inserter ---"
+curl -d "@examples/inserter.json" -i -H "Accept: text/plain" -H "Content-Type: application/json; charset=UTF-8" -X PUT "$SERVER/inserter/reuters"
 
 echo "--- Insert documents ---"
 TRANSACTION=`curl -H "Accept: text/plain" -X POST "$SERVER/inserter/reuters/transaction"`
