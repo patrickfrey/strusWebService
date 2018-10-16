00eaa548cd956b3e76b9d21c939d28f7d397955f
patrickfrey <patrickpfrey@yahoo.com>
2018-10-16 15:16:36 +0200

    example buildAll script: server as variable

--- examples/buildAll.sh
+++ examples/buildAll.sh
@@ -1,30 +1,34 @@
 #!/bin/sh
 
+SERVER="http://127.0.0.1:8080"
+
 echo "--- Create analyzer ---"
-curl -d "@examples/docanalyzer.json" -i -H "Accept: text/plain" -H "Content-Type: application/json; charset=UTF-8" -X PUT "http://127.0.0.1:8080/docanalyzer/reuters"
+curl -d "@examples/docanalyzer.json" -i -H "Accept: text/plain" -H "Content-Type: application/json; charset=UTF-8" -X PUT "$SERVER/docanalyzer/reuters"
 
 echo "--- Create storage ---"
-curl -d "@examples/storage.json" -i -H "Accept: text/plain" -H "Content-Type: application/json; charset=UTF-8" -X PUT "http://127.0.0.1:8080/storage/reuters"
+curl -d "@examples/storage.json" -i -H "Accept: text/plain" -H "Content-Type: application/json; charset=UTF-8" -X PUT "$SERVER/storage/reuters"
 
 echo "--- Create inserter ---"
-curl -d "@examples/inserter.json" -i -H "Accept: text/plain" -H "Content-Type: application/json; charset=UTF-8" -X PUT "http://127.0.0.1:8080/inserter/reuters"
+curl -d "@examples/inserter.json" -i -H "Accept: text/plain" -H "Content-Type: application/json; charset=UTF-8" -X PUT "$SERVER/inserter/reuters"
 
 echo "--- Create content statistics analyzer ---"
-curl -d "@examples/contentstats.json" -i -H "Accept: text/plain" -H "Content-Type: application/json; charset=UTF-8" -X PUT "http://127.0.0.1:8080/contentstats/reuters"
+curl -d "@examples/contentstats.json" -i -H "Accept: text/plain" -H "Content-Type: application/json; charset=UTF-8" -X PUT "$SERVER/contentstats/reuters"
 
 echo "--- Analyze documents ---"
-TRANSACTION=`curl -H "Accept: text/plain" -X POST "http://127.0.0.1:8080/contentstats/reuters/transaction"`
+TRANSACTION=`curl -H "Accept: text/plain" -X POST "$SERVER/contentstats/reuters/transaction"`
 for dd in `find tests/data/reuters/doc/ -name "reut2-000.xml"`; do
 	echo "--- Collect statistics $dd ---"
 	curl -d "@$dd" -i -H "Accept: text/plain" -H "Content-Type: application/xml; charset=UTF-8" -X PUT "$TRANSACTION"
 done
 
 echo "--- Insert documents ---"
-TRANSACTION=`curl -H "Accept: text/plain" -X POST "http://127.0.0.1:8080/inserter/reuters/transaction"`
+TRANSACTION=`curl -H "Accept: text/plain" -X POST "$SERVER/inserter/reuters/transaction"`
 for dd in `find tests/data/reuters/doc/ -name "*.xml"`; do
 	echo "--- Insert $dd ---"
 	curl -d "@$dd" -i -H "Accept: text/plain" -H "Content-Type: application/xml; charset=UTF-8" -X PUT "$TRANSACTION"
 done
 curl -i -H "Accept: text/plain" -H "Content-Type: application/xml; charset=UTF-8" -X PUT "$TRANSACTION"
 
+echo "--- Query ---"
+curl -d "@examples/query.orig.json" -i -H "Accept: application/json" -H "Content-Type: application/json; charset=UTF-8" -X GET  "$SERVER/storage/reuters"
 
