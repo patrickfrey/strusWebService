38cceec59e64262e865ebbbef2b892442a8be8b6
PatrickFrey <patrickpfrey@yahoo.com>
2019-03-11 12:51:11 +0100

    updated travis build script (outdated as never tested)

--- dist/travis/script.sh
+++ dist/travis/script.sh
@@ -6,29 +6,82 @@ OS=$(uname -s)
 
 PROJECT=strusWebService
 
-# set up environment
-case $OS in
-	Linux)
-		;;
+build_dep_project() {
+	prj_cmakeflags=$1
+	case $OS in
+		Linux)
+			mkdir build
+			cd build
+			cmake -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Release \
+				-DLIB_INSTALL_DIR=lib -DCMAKE_CXX_FLAGS=-g $prj_cmakeflags \
+				..
+			make
+			sudo make install
+			cd ..
+			;;
+
+		Darwin)
+			# forcing brew versions (of gettext) over Mac versions
+			export CFLAGS=-I/usr/local
+			export CXXFLAGS=-I/usr/local
+			export LDFLAGS=-L/usr/local/lib
+			mkdir build
+			cd build
+			cmake \
+				-DCMAKE_INSTALL_PREFIX=/usr/local -DCMAKE_BUILD_TYPE=Release \
+				-DCMAKE_CXX_FLAGS='-g -Wno-error=format-nonliteral -Wno-error=format-security' -G Xcode $prj_cmakeflags \
+				..
+			xcodebuild -configuration Release -target ALL_BUILD
+			sudo xcodebuild -configuration Release -target install
+			cd ..
+			;;
+			
+		*)
+			echo "ERROR: unknown operating system '$OS'."
+			;;
+	esac
+}
+
+build_strus_project() {
+	prj_cmakeflags=$1
+	case $OS in
+		Linux)
+			mkdir build
+			cd build
+			cmake -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Release \
+				-DLIB_INSTALL_DIR=lib -DCMAKE_CXX_FLAGS=-g $prj_cmakeflags \
+				..
+			make VERBOSE=1
+			make VERBOSE=1 CTEST_OUTPUT_ON_FAILURE=1 test
+			sudo make VERBOSE=1 install
+			cd ..
+			;;
 	
-	Darwin)
-		if test "X$CC" = "Xgcc"; then
-			# gcc on OSX is a mere frontend to clang, force using gcc 4.8
-			export CXX=g++-4.8
-			export CC=gcc-4.8
-		fi
-		# forcing brew versions (of gettext) over Mac versions
-		export CFLAGS="-I/usr/local"
-		export CXXFLAGS="-I/usr/local"
-		export LDFLAGS="-L/usr/local/lib"
-		;;
+		Darwin)
+			# forcing brew versions (of gettext) over Mac versions
+			export CFLAGS=-I/usr/local
+			export CXXFLAGS=-I/usr/local
+			export LDFLAGS=-L/usr/local/lib
+			mkdir build
+			cd build
+			cmake \
+				-DCMAKE_INSTALL_PREFIX=/usr/local -DCMAKE_BUILD_TYPE=Release \
+				-DCMAKE_CXX_FLAGS='-g -Wno-error=format-nonliteral -Wno-error=format-security' -G Xcode $prj_cmakeflags \
+				..
+			xcodebuild -configuration Release -target ALL_BUILD
+			xcodebuild -configuration Release -target RUN_TESTS
+			sudo xcodebuild -configuration Release -target install
+			cd ..
+			;;
+			
+		*)
+			echo "ERROR: unknown operating system '$OS'."
+			;;
+	esac
+}
 
-	*)
-		echo "ERROR: unknown operating system '$OS'."
-		;;
-esac
+# build pre-requisites
 
-# build cppcms
 cd ..
 wget https://sourceforge.net/projects/cppcms/files/cppcms/1.0.5/cppcms-1.0.5.tar.bz2
 bzip2 -d cppcms-1.0.5.tar.bz2
@@ -38,113 +91,30 @@ cmake .
 sudo make install
 cd $PROJECT
 
-# build pre-requisites
 DEPS="strusBase strus strusAnalyzer strusTrace strusModule"
-
+if test "x$STRUS_WITH_PATTERN" = "xYES"; then
+	DEPS="$DEPS strusPattern"
+fi
+if test "x$STRUS_WITH_VECTOR" = "xYES"; then
+	DEPS="$DEPS strusVector"
+fi
 GITURL=`git config remote.origin.url`
 cd ..
 for i in $DEPS; do
 	git clone `echo $GITURL | sed "s@/$PROJECT\.@/$i.@g"` $i
 	cd $i
-	git checkout travis
-	case $OS in
-		Linux)
-			mkdir build
-			cd build
-			cmake -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Release \
-				-DLIB_INSTALL_DIR=lib -DCMAKE_CXX_FLAGS=-g \
-				..
-			make VERBOSE=1
-			make VERBOSE=1 test
-			sudo make VERBOSE=1 install
-			cd ..
-			;;
-		
-		Darwin)
-			if test "X$CC" = "Xgcc-4.8"; then
-				mkdir build
-				cd build
-				cmake \
-					-DCMAKE_INSTALL_PREFIX=/usr/local -DCMAKE_BUILD_TYPE=Release \
-					-DCMAKE_CXX_FLAGS=-g -G 'Unix Makefiles' \
-					..
-				make VERBOSE=1
-				make VERBOSE=1 test
-				sudo make VERBOSE=1 install
-				cd ..
-			else
-				mkdir build
-				cd build
-				cmake \
-					-DCMAKE_INSTALL_PREFIX=/usr/local -DCMAKE_BUILD_TYPE=Release \
-					-DCMAKE_CXX_FLAGS=-g -G Xcode \
-					..
-				xcodebuild -configuration Release -target ALL_BUILD
-				xcodebuild -configuration Release -target RUN_TESTS
-				sudo xcodebuild -configuration Release -target install
-				cd ..
-			fi
-			;;
+	git submodule update --init --recursive
+	git submodule foreach --recursive git checkout master
+	git submodule foreach --recursive git pull
 
-		*)
-			echo "ERROR: unknown operating system '$OS'."
-			;;
-	esac
+	build_strus_project ""
 	cd ..
 done
-cd $PROJECT
 
 # build the package itself
-case $OS in
-	Linux)
-		mkdir build
-		cd build
-		cmake -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Release \
-			-DLIB_INSTALL_DIR=lib -DCMAKE_CXX_FLAGS=-g \
-			..
-		make VERBOSE=1
-		make run &
-		sleep 10
-		make VERBOSE=1 test
-		pkill strusWebService
-		sudo make VERBOSE=1 install
-		cd ..
-		;;
+cd $PROJECT
+echo "BUILD $PROJECT"
+build_strus_project ""
+cd ..
 
-	Darwin)
-		if test "X$CC" = "Xgcc-4.8"; then
-			mkdir build
-			cd build
-			cmake \
-				-DCMAKE_INSTALL_PREFIX=/usr/local -DCMAKE_BUILD_TYPE=Release \
-				-DCMAKE_CXX_FLAGS=-g -G 'Unix Makefiles' \
-				..
-			make VERBOSE=1
-			make run &
-			sleep 10
-			make VERBOSE=1 test
-			pkill strusWebService
-			sudo make VERBOSE=1 install
-			cd ..
-		else
-			mkdir build
-			cd build
-			cmake \
-				-DCMAKE_INSTALL_PREFIX=/usr/local -DCMAKE_BUILD_TYPE=Release \
-				-DCMAKE_CXX_FLAGS=-g -G Xcode \
-				..
-			xcodebuild -configuration Release -target ALL_BUILD
-			Release/strusWebService -v -c ../config.js &
-			sleep 10
-			xcodebuild -configuration Release -target RUN_TESTS
-			pkill strusWebService
-			sudo xcodebuild -configuration Release -target install
-			cd ..
-		fi
-		;;
-		
-	*)
-		echo "ERROR: unknown operating system '$OS'."
-		;;
-esac
-	
+
